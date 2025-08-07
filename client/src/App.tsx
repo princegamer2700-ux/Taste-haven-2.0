1d0832875057a7e930e3b3a15a8f02de905c5b19
princegamer2700-ux <princegamer2700@gmail.com>
2025-08-07 15:43:47 +0530

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,42 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/home";
+import Menu from "@/pages/menu";
+import Cart from "@/pages/cart";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/menu" component={Menu} />
+      <Route path="/cart" component={Cart} />
+      {/* Fallback to 404 */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <main className="flex-grow">
+            <Router />
+          </main>
+          <Footer />
+        </div>
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
